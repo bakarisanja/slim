3d36efa8eb7c0182f42de0b31292b3dd1facbd11
bakarisanja <milos.babic@mi-system.co.rs>
2015-09-04 15:40:57 +0200

    dsfsdfsdf

---
+++ config/database.php
@@ -0,0 +1,31 @@
+<?php
+return array(
+	'default'	=> array(
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'teasedb',
+		'port'     => ''
+		),
+	'account_opening'		=> array(
+		'hostname'		=> '10.200.136.16',
+		'username'		=> 'userNewApps',
+		'password'		=> 'spp@we$resu',
+		'database'		=> 'NewApps',
+		'port'		=> 3306
+		),
+	'internal_wp'	=> array(
+		'hostname'		=> '10.200.136.13',
+		'username'		=> 'wpadmin',
+		'password'		=> 'Password!',
+		'database'		=> 'wordpress',
+		'port'		=> 3306
+		),
+	'sugar_crm'	=> array(
+		'hostname'		=> '10.200.31.40',
+		'username'		=> 'sug4crm',
+		'password'		=> 'T1mm4h5!',
+		'database'		=> 'sug4crm',
+		'port'		=> 3306
+		)
+	);
